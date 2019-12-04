d9133a297e6ae4e102ef53fe8bfe750f88702ce2
aRtoo <raphaelg2012@gmail.com>
2019-12-03 21:14:02 -0800

    added models and trying models, created basemodel

---
+++ src/models/user_model.ts
@@ -0,0 +1,20 @@
+import { BaseModel } from "./base_model";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+/**
+ * users table name
+ */
+@Entity('users')
+export class UserModel extends BaseModel {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  username!: string
+
+  @Column()
+  email!: string
+
+  @Column()
+  password!: string
+}
