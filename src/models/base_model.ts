e734eb228585848f222431e63ab8ba5b198ff347
aRtoo <raphaelg2012@gmail.com>
2020-01-20 17:14:18 -0800

    change validation model from validate to validateOrReject

--- src/models/base_model.ts
+++ src/models/base_model.ts
@@ -1,14 +1,14 @@
 import { BaseEntity } from 'typeorm';
-import { validate } from 'class-validator';
+import { validateOrReject } from 'class-validator';
 
 export class BaseModel extends BaseEntity {
   async validateModel() {
-    // this error returns an array
-    const errors = await validate(this);
-    if (errors.length > 0) {
-      throw errors;
-    } else {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      await validateOrReject(this, { validationError: { target: false } });
       return this;
+    } catch (error) {
+      throw error;
     }
   }
 }
