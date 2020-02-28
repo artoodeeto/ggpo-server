99584665d779c930a1a9425da5f49495ab896a97
raphael <raphaelg2012@gmail.com>
2020-02-27 20:34:03 -0800

    updated eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,45 +2,40 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    "jest": true
+    jest: true
   },
-  extends: [
-    'airbnb-base',
-    'plugin:import/typescript',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['airbnb-base', 'plugin:import/typescript', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   ignorePatterns: ['node_modules'],
   rules: {
-    "ignoreExport": 0,
-    "import/prefer-default-export": 0,
-    "no-console": 2,
-    "comma-dangle": 0,
-    "import/first": 0,
-    "no-unused-vars": 1,
-    "consistent-return": 2,
-    "import/no-mutable-exports": 1,
-    "no-undef": 1,
-    "import/order": 0
+    ignoreExport: 0,
+    'import/prefer-default-export': 0,
+    'no-console': 2,
+    'comma-dangle': 0,
+    'import/first': 0,
+    'no-unused-vars': 1,
+    'consistent-return': 2,
+    'import/no-mutable-exports': 1,
+    'no-undef': 1,
+    'import/order': 0,
+    'import/no-cycle': 0,
+    'dot-notation': 0
   },
   overrides: [
     {
       files: ['src/controllers/*.ts'], // overiding the rule for this file because overnightjs doesnt use the methods in its class
       excludedFiles: [],
       rules: {
-        "class-methods-use-this": 0
+        'class-methods-use-this': 0
       }
     }
   ]
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,10 @@
-module.exports =  {
-  semi:  true,
-  trailingComma:  'none',
-  singleQuote:  true,
-  printWidth:  120,
-  tabWidth:  2,
+module.exports = {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
   bracketSpacing: true,
-  arrowParens: 'always'
+  arrowParens: 'always',
+  tabs: false
 };
