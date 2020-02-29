5dec1d32ddddbb5c18d3f78b0216089c0e5906ca
aRtoo <raphaelg2012@gmail.com>
2020-02-29 07:41:34 -0800

    Feat/post (#23)
    * updated eslint and prettier
    
    * commented initdb
    
    * done with post, added tests

---
+++ src/models/post.ts
@@ -0,0 +1,39 @@
+import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn, ManyToOne, BeforeInsert } from 'typeorm';
+import { User } from './user';
+import { BaseModel } from './base_model';
+
+@Entity({ name: 'posts' })
+export class Post extends BaseModel {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  title!: string;
+
+  @Column({ type: 'text', width: 65000 })
+  body!: string;
+
+  @Column({ type: 'datetime' })
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  // @Column({ type: 'datetime' })
+  // deletedAt!: Date;
+
+  @ManyToOne(
+    (type) => User,
+    (user) => user.posts
+  )
+  user!: User;
+
+  /**
+   * a work around to @CreateDateColumn()
+   * TODO: move this to base class
+   */
+  @BeforeInsert()
+  private async setCreatedAtDate() {
+    this.createdAt = new Date();
+  }
+}
---
+++ test/controllers/post.controller.test.ts
@@ -0,0 +1,166 @@
+import request from 'supertest';
+import { Connection, createConnection } from 'typeorm';
+import { AppServer } from '../../config/server';
+import { User } from '../../src/models/user';
+import { testSetup } from '../../config/test_setup';
+
+const server = new AppServer();
+const { appInstance } = server;
+const rekwest = request(appInstance);
+
+describe('Post controllers', () => {
+  let connection: Connection;
+  let ACTIVE_JWT: string;
+  let createPost: Function;
+  const userInfo = {
+    username: 'nocap',
+    email: 'nocap@gmail.com',
+    password: 'password'
+  };
+  const samplePost = {
+    title: 'shit',
+    body: 'bull'
+  };
+  const EXPIRED_HEADER =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJub2NhcEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6Im5vY2FwIiwiaWF0IjoxNTgwODc0OTMxLCJleHAiOjE1ODA4ODU3MzF9.-f9zq8LdOwdCuwZkS_T1oyFOoxIVJ5lSv5zWHClOiUs';
+
+  beforeEach(async () => {
+    connection = await createConnection(testSetup);
+    const res = await rekwest.post('/api/v1/users/signup').send({ ...userInfo });
+    ACTIVE_JWT = res.body.payload.token;
+    createPost = (): Promise<any> => {
+      return rekwest
+        .post('/api/v1/posts')
+        .send({ title: 'title', body: 'body' })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+    };
+  });
+
+  afterEach(async () => {
+    connection.close();
+  });
+
+  describe('POST: /posts route', () => {
+    test('should success response on creating a post', async () => {
+      const res = await rekwest
+        .post('/api/v1/posts')
+        .send({ ...samplePost })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(200);
+    });
+
+    test('should fail if given null values', async () => {
+      const res = await rekwest
+        .post('/api/v1/posts')
+        .send({ title: null, body: null })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(400);
+    });
+  });
+
+  describe('GET: /posts/:id route', () => {
+    test('should success in getting specific posts', async () => {
+      const newPost = await createPost();
+      const res = await rekwest
+        .get(`/api/v1/posts/${newPost.body.payload.post.id}`)
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(200);
+      expect(res.body.payload.post).toContainKeys(['id', 'body', 'title']);
+    });
+
+    test('should fail if no ID is given', async () => {
+      const res = await rekwest.get('/api/v1/posts/').set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(404);
+    });
+
+    test('should fail if ID is in correct', async () => {
+      const res = await rekwest.get('/api/v1/posts/123123123123').set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe('PUT: /posts/:id route', () => {
+    test('should update a post', async () => {
+      const newPost = await createPost();
+      const { id } = newPost.body.payload.post;
+      const res = await rekwest
+        .put(`/api/v1/posts/${id}`)
+        .send({ title: 'the', body: 'new' })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(200);
+      const { title, body } = res.body.payload.post;
+      expect(title).toBe('the');
+      expect(body).toBe('new');
+    });
+
+    test('should fail if no ID is given', async () => {
+      const res = await rekwest
+        .put('/api/v1/posts/')
+        .send({ ...samplePost })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(404);
+    });
+
+    test('should fail if ID is in correct', async () => {
+      const res = await rekwest
+        .put('/api/v1/posts/123123123123')
+        .send({ ...samplePost })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe('DELETE: /posts/:id route', () => {
+    test('should delete a post', async () => {
+      const newPost = await createPost();
+      const { id } = newPost.body.payload.post;
+      const res = await rekwest
+        .delete(`/api/v1/posts/${id}`)
+        .send({ title: 'the', body: 'new' })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(200);
+    });
+
+    test('should fail if no ID is given', async () => {
+      const res = await rekwest
+        .delete('/api/v1/posts/')
+        .send({ ...samplePost })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(404);
+    });
+
+    test('should fail if ID is in correct', async () => {
+      const res = await rekwest
+        .delete('/api/v1/posts/123123123123')
+        .send({ ...samplePost })
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe('GET: /posts/paginated/query', () => {
+    test('should get success response', async () => {
+      const res = await rekwest.get('/api/v1/posts/paginated/query').set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.status).toBe(200);
+    });
+
+    test('should get 5 post', async () => {
+      await createPost();
+      await createPost();
+      await createPost();
+      await createPost();
+      await createPost();
+      await createPost();
+      await createPost();
+      const res = await rekwest
+        .get('/api/v1/posts/paginated/query?limit=5')
+        .set('Authorization', `Bearer ${ACTIVE_JWT}`);
+      expect(res.body.payload.posts).toHaveLength(5);
+    });
+
+    test('should fail if token is invalid', async () => {
+      const res = await rekwest.get('/api/v1/posts/paginated/query').set('Authorization', `Bearer ${EXPIRED_HEADER}`);
+      expect(res.status).toBe(401);
+    });
+  });
+});
