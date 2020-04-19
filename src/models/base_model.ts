3725e40f355019018b656f8ca94f3daee7f588ff
raphael <raphaelg2012@gmail.com>
2020-04-18 19:59:09 -0700

    refactor model validation. removed unnecessary throw

--- src/models/base_model.ts
+++ src/models/base_model.ts
@@ -1,14 +1,9 @@
-/* eslint-disable no-useless-catch */
 import { BaseEntity } from 'typeorm';
 import { validateOrReject } from 'class-validator';
 
 export class BaseModel extends BaseEntity {
   async validateModel(): Promise<BaseModel> {
-    try {
-      await validateOrReject(this, { validationError: { target: false } });
-      return this;
-    } catch (error) {
-      throw error;
-    }
+    await validateOrReject(this, { validationError: { target: false } });
+    return this;
   }
 }
