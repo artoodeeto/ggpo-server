996bc0da4c30902b34c2e6d57d438da00fc3c77d
raphael <raphaelg2012@gmail.com>
2020-05-16 19:10:21 -0700

    fixed import names

--- src/controllers/version_one_controller.ts
+++ src/controllers/version_one_controller.ts
@@ -2,12 +2,12 @@ import { Controller, ChildControllers } from '@overnightjs/core';
 import { UsersController } from './users_controller';
 import { BaseController } from './base_controller';
 import { PostsController } from './posts_controller';
-import { SessionController } from './sessions_controller';
+import { SessionsController } from './sessions_controller';
 import { GameGroupsController } from './game_groups_controller';
 
 /**
  * Use this for client API
  */
 @Controller(`${process.env.PREFIX}/${process.env.API_VERSION}`)
-@ChildControllers([new UsersController(), new PostsController(), new SessionController(), new GameGroupsController()])
+@ChildControllers([new UsersController(), new PostsController(), new SessionsController(), new GameGroupsController()])
 export class VersionOne extends BaseController {}
