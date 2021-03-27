02d602b48d43e1490e1b37e64a46e5e6f817bee5
raphael <raphaelg2012@gmail.com>
2021-03-26 20:43:28 -0700

    fix merge conflicts

--- config/swagger.ts
+++ config/swagger.ts
@@ -1,4 +1,4 @@
 import fs from 'fs';
 import YAML from 'js-yaml';
 
-export const swaggerDocument = YAML.load(fs.readFileSync('docs/api-docs.yml').toString());
+export const swaggerDocument: any = YAML.load(fs.readFileSync('docs/api-docs.yml').toString());
