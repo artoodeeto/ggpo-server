4191b3122620cd191d23e48727975032ff382ffa
raphael <raphaelg2012@gmail.com>
2021-07-16 21:56:39 -0700

    WIP: passport not working

--- src/errors/uncaughtException.ts
+++ src/errors/uncaughtException.ts
@@ -3,7 +3,7 @@ import { logger } from '../../config/logger';
 // sync errors, all 500 errors are here
 export const uncaughtException = () => {
   process.on('uncaughtException', (error: Error): void => {
-    logger.error({ error }, 'uncaughtException:');
+    logger.error(error, 'uncaughtException:');
     // process.exit(1);
   });
 };
